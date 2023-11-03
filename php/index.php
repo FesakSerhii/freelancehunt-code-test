e429e90e42a879510edee7b65da0fa24cd93c70d
serhiyfesak <serhiyfesak@gmail.com>
2023-11-03 22:39:24 +0200

    fix cors

--- php/index.php
+++ php/index.php
@@ -1,14 +1,20 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:5173"); // Замініть на фактичний домен вашого клієнта
-
-header("Access-Control-Allow-Credentials: true");
-
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit();
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
 }
 
 
